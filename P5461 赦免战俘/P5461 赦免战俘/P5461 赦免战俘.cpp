e92dec5bde183bc15b7f83cba860695868b68521
aloys23 <aloys233@gmail.com>
2023-12-18 23:21:46 +0800

    Push Code:周一 2023/12,23:21:46

--- P5461 赦免战俘/P5461 赦免战俘/P5461 赦免战俘.cpp
+++ P5461 赦免战俘/P5461 赦免战俘/P5461 赦免战俘.cpp
@@ -1,33 +1,57 @@
-﻿	#include<iostream>
-	int main() {
-		int n;
-		std::cin >> n;
-		int matrixSize = 1 << n;
-		int** arr = new int* [matrixSize];
-		for (int i = 0; i < matrixSize; i++) {
-			arr[i] = new int[matrixSize]();
-		}
-		for (int y = 0; y < matrixSize; ++y) {
-			for (int x = 0; x < matrixSize; ++x) {
-				int bit = 0;
-				for (int k = 0;k<matrixSize; ++k) {
-					if ((x & (1 << k)) && (y & (1 << k))) {
-						bit = 1;
-						break;
-					}
-				}
-				arr[y][x] = bit;
-			}
-		}
-		for (int i = 0; i < matrixSize; i++) {
-			for (int j = 0; j < matrixSize; j++) {
-				std::cout << arr[i][j] << " ";
-			}
-			std::cout << std::endl;
-		}
-		for (int i = 0; i < matrixSize; i++) {
-			delete[] arr[i];
-		}
-		delete[] arr;
-		return 0;
-	}
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// 递归函数，根据题目描述进行赦免或处罚
+void processCheaters(vector<vector<int>>& matrix, int row, int col, int size) {
+    if (size == 1) {
+        return;  // 已经是最小的矩阵，无法再分
+    }
+
+    int half = size / 2;
+
+    // 处理左上角的矩阵
+    processCheaters(matrix, row, col, half);
+
+    // 处理右上角的矩阵
+    processCheaters(matrix, row, col + half, half);
+
+    // 处理左下角的矩阵
+    processCheaters(matrix, row + half, col, half);
+
+    // 处理右下角的矩阵
+    processCheaters(matrix, row + half, col + half, half);
+
+    // 如果左上角的矩阵中没有被赦免的作弊者，则将右上、左下、右下的矩阵中的作弊者处罚
+    if (matrix[row][col] == 1) {
+        for (int i = row; i < row + size; ++i) {
+            for (int j = col; j < col + size; ++j) {
+                matrix[i][j] = 1;
+            }
+        }
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    int size = 1 << n;  // 计算矩阵的大小
+
+    // 初始化矩阵
+    vector<vector<int>> matrix(size, vector<int>(size));
+
+    // 处理矩阵
+    processCheaters(matrix, 0, 0, size);
+
+    // 输出结果
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
---
+++ 圆结构体/圆结构体/圆结构体.cpp
@@ -0,0 +1,19 @@
+﻿#define _USE_MATH_DEFINES
+#include<cmath>
+#include<iostream>
+class circle {
+private:
+	int r;//边长
+public:
+	circle(int rr) :r(rr) {};
+	int getarea() {
+		return M_PI * r * r;
+	}
+};
+int main() {
+	int a;
+	std::cin >> a;
+	circle aaa(a);
+	std::cout << aaa.getarea() << std::endl;
+	return 0;
+}
