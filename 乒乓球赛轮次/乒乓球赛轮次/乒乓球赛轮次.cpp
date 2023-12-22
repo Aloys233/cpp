5a6278db688e735b04383efe1aa4a1ddfe6d721b
aloys23 <aloys233@gmail.com>
2023-12-22 15:55:42 +0800

    Push Code:周五 2023/12,15:55:41

---
+++ 乒乓球赛轮次/乒乓球赛轮次/乒乓球赛轮次.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main() {
+    int t;
+    std::cin>>t;
+    int input;
+    for(int i=0;i<t;i++){
+        std::cin >> input;
+        if(input%2==0){
+            std::cout << (int)(log2(input) + 0.5);
+        }
+        else{
+            std::cout << (int)(log2(input - 1) + 0.5);
+        }
+        if(i<t-1){
+            std::cout<<std::endl;
+        }
+    }
+    return 0;
+}
