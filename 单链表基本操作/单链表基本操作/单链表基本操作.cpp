f4b3091e1bcf25ec006b16545f54f3eff1c3f289
aloys23 <aloys233@gmail.com>
2024-04-12 22:00:56 +0800

    Push Code:周五 2024/04,22:00:56

---
+++ 单链表基本操作/单链表基本操作/单链表基本操作.cpp
@@ -0,0 +1,94 @@
+﻿#include<iostream>
+#include<cstdlib>
+#include<iomanip>
+#define MAXSIZE 100
+#define OK 1
+#define ERROR 0
+#define OVERFLOW -2
+using namespace std;
+
+typedef int Status;
+
+typedef struct LNode {
+	int data;
+	struct LNode* next;
+}LNode, * LinkList;
+
+Status InitList(LinkList& L) {
+	L = new LNode;
+	L->next = NULL;
+	return OK;
+}
+
+Status ListInsert(LinkList& L, int i, int e) {
+	LNode* p = L;
+	int j = 0;
+	while (p && j < i - 1) {
+		p = p->next;
+		j++;
+	}
+	if (!p || j > i - 1) {
+		return ERROR;
+	}
+	LNode* s = new LNode;
+	s->data = e;
+	s->next = p->next;
+	p->next = s;
+	return OK;
+}
+
+Status ListDelete(LinkList& L, int i) {
+	LNode* P = L;
+	int j = 0;
+	while (P->next && j < i - 1) {
+		P = P->next;
+		j++;
+	}
+	if (!(P->next) || j > i - 1) {
+		return ERROR;
+	}
+	LNode* q = P->next;
+	P->next = q->next;
+	delete q;
+	return OK;
+}
+
+Status ListOutput(LinkList L) {
+	LinkList p = L->next;
+	if (!p) {
+		return ERROR;
+	}
+	while (p) {
+		cout << p->data << " ";
+		p = p->next;
+	}
+	cout << endl;
+	return OK;
+}
+
+int main() {
+	int n, m, op, k, d;
+	cin >> n;//输入链表长度
+	LinkList L;
+	InitList(L);
+	for (int i = 0; i < n; i++) {
+		cin >> m;
+		ListInsert(L, i + 1, m);
+	}
+	cin >> m;//输入操作次数
+	for (int i = 0; i < m; i++) {
+		cin >> op;
+		if (op == 0) {
+			cin >> k >> d;
+			ListInsert(L, k + 1, d);
+		}
+		else if (op == 1) {
+			cin >> k;
+			if (k != 0) {
+				ListDelete(L, k);
+			}
+		}
+	}
+	ListOutput(L);
+	return 0;
+}
---
+++ 在有序链表中插入数据/在有序链表中插入数据/在有序链表中插入数据.cpp
@@ -0,0 +1,92 @@
+﻿#include<iostream>
+#include<cstdlib>
+#include<iomanip>
+#define MAXSIZE 100
+#define OK 1
+#define ERROR 0
+#define OVERFLOW -2
+using namespace std;
+
+typedef int Status;
+
+typedef struct LNode {
+	int data;
+	struct LNode* next;
+}LNode, * LinkList;
+
+Status InitList(LinkList& L) {
+	L = new LNode;
+	L->next = NULL;
+	return OK;
+}
+
+Status ListInsert(LinkList& L, int i, int e) {
+	LNode* p = L;
+	int j = 0;
+	while (p && j < i - 1) {
+		p = p->next;
+		j++;
+	}
+	if (!p || j > i - 1) {
+		return ERROR;
+	}
+	LNode* s = new LNode;
+	s->data = e;
+	s->next = p->next;
+	p->next = s;
+	return OK;
+}
+
+Status ListOutput(LinkList L) {
+	LinkList p = L->next;
+	if (!p) {
+		return ERROR;
+	}
+	while (p) {
+		cout << p->data;
+		if (p->next) {
+			cout << " ";
+		}
+		p = p->next;
+	}
+	cout << endl;
+	return OK;
+}
+
+void InsertMerge(LinkList& L, int e) {
+	int j = 0;
+	LNode* current = L->next;
+	bool a = false;
+	while (current != NULL) {
+		if (current->data > e) {
+			ListInsert(L, j + 1, e);
+			a = true;
+			break;
+		}
+		else if (current->data == e) {
+			a = true;
+			break;
+		}
+		current = current->next;
+		j++;
+	}
+	if (!a) {
+		ListInsert(L, j + 1, e);
+	}
+}
+
+int main() {
+	LinkList L;
+	InitList(L);
+	int n,a;
+	cin>> n;
+	for (int i = 0; i < n; i++) {
+		int e;
+		cin >> e;
+		ListInsert(L, i + 1, e);
+	}
+	cin >> a;
+	InsertMerge(L, a);
+	ListOutput(L);
+	return 0;
+}
