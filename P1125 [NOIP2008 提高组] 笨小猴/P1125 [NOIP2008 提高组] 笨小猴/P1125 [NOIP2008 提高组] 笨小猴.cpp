ae474b612bf16da478e7ea96dd8e84fee21ee4d5
aloys23 <aloys233@gmail.com>
2024-06-04 15:19:50 +0800

    Push Code:周二 2024/06,15:19:50

---
+++ P1125 [NOIP2008 提高组] 笨小猴/P1125 [NOIP2008 提高组] 笨小猴/P1125 [NOIP2008 提高组] 笨小猴.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int IsPrime(int num) {
+    if (num < 2) {
+        return 0; //不是素数
+    }
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return 0; //不是素数
+        }
+    }
+    return 1; //是素数
+}
+
+int MaxTimes(string n) {
+    int count[26] = {0};
+    for (int i = 0; i < n.size(); i++) {
+        count[n[i] - '`']++;
+    }
+    int max_count = 0;
+    char max_char = 0;
+    for (int i = 0; i < 26; i++) {
+        if (count[i] > max_count) {
+            max_count = count[i];
+            max_char = i + '`';
+        }
+    }
+    return max_count;
+}
+
+int MinTimes(string n) {
+    int count[26] = { 0 };
+    for (int i = 0; i < n.size(); i++) {
+        count[n[i] - '`']++;
+    }
+    int min_count = 10e9;
+    char min_char = 0;
+    for (int i = 0; i < 26; i++) {
+        if (count[i] != 0 && count[i] < min_count) {
+            min_count = count[i];
+            min_char = i + '`';
+        }
+    }
+    return min_count;
+}
+
+int main() {
+    string a;
+    cin >> a;
+    if (IsPrime(MaxTimes(a) - MinTimes(a))){
+        cout << "Lucky Word" << endl << MaxTimes(a) - MinTimes(a);
+    }
+    else {
+        cout << "No Answer" << endl << "0";
+    }
+    return 0;
+}
---
+++ P1271 【深基9.例1】选举学生会/P1271 【深基9.例1】选举学生会/P1271 【深基9.例1】选举学生会.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	int* a = new int[m];
+	for (int i = 0; i < m; i++) {
+		int e;
+		cin >> e;
+		if (e <= n) {
+			a[i] = e;
+		}
+	}
+	sort(a, a + m);
+	for (int i = 0; i < m; i++) {
+		cout << a[i] << " ";
+	}
+	return 0;
+}
