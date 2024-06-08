7f17335191534bbf05fbf0b8a91a09b23a651b0d
aloys23 <aloys233@gmail.com>
2024-06-08 22:00:26 +0800

    Push Code:周六 2024/06,22:00:26

---
+++ P1601 A+B Problem（高精）/P1601 A+B Problem（高精）/P1601 A+B Problem（高精）.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+	vector<int> a, b, c;
+	int t = 0;
+	string input;
+	getline(cin, input);
+	for (int i = input.length() - 1; i >= 0; i--) {
+		a.push_back(input[i] - '0');
+	}
+	getline(cin, input);
+	for (int i = input.length() - 1; i >= 0; i--) {
+		b.push_back(input[i] - '0');
+	}
+	int maxSize = max(a.size(), b.size());
+	for (int i = 0; i < maxSize || t; i++) {
+		if (i < a.size()) {
+			t += a[i];
+		}
+		if (i < b.size()) {
+			t += b[i];
+		}
+		c.push_back(t % 10);
+		t /= 10;
+	}
+	for (int i = c.size() - 1; i >= 0; i--) {
+		cout << c[i];
+	}
+	return 0;
+}
